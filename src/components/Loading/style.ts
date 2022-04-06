87d7df40c974cc80ba7d95ce02f39eab7ba3038d
Parksangwoo <psu8430@gmail.com>
2022-04-07 03:41:49 +0900

    Feat: Loaing 컴포넌트 추가

---
+++ src/components/Loading/style.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const LoadingBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #000000a8;
+`;
+
+export const LoadingContentWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  img {
+    background: white;
+    border-radius: 50px;
+    box-shadow: 0 0 20px 0 black;
+  }
+`;
+
+export const LoadingText = styled.p`
+  margin: 0;
+  margin-top: 20px;
+  text-align: center;
+  font-size: 18px;
+  font-weight: 500;
+  animation-duration: 1.5s;
+  animation-name: nameColor;
+  animation-iteration-count: infinite;
+
+  @keyframes nameColor {
+    0% {
+      color: #ffffff;
+    }
+    50% {
+      color: #909090;
+    }
+    100% {
+      color: #ffffff;
+    }
+  }
+`;
