030064b77582efa9f0a81c297e9f15af28f88ff2
Parksangwoo <psu8430@gmail.com>
2022-04-08 02:49:56 +0900

    Design: 폰트 파일 추가

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,6 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   /* reset */
+@font-face {
+  font-family: "PKMN RBYGSC";
+  src: url("./assets/static/font/PKMN RBYGSC.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "KARMATIC ARCADE";
+  src: url("./assets/static/font/KARMATIC ARCADE.ttf") format("truetype");
+}
+
   *{margin:0;padding:0;font:inherit;color:inherit;}
   *,:after, :before {box-sizing:border-box;flex-shrink:0;}
   :root {-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;text-size-adjust:100%;cursor:default;line-height:1.5;overflow-wrap:break-word;-moz-tab-size:4;tab-size:4}
