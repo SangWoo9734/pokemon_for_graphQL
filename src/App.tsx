92ac6128e7c596a020c5cb334a14622ec20bf3f0
Parksangwoo <psu8430@gmail.com>
2022-04-09 03:04:02 +0900

    Feat: Redux Store 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Route, Routes } from "react-router";
 import styled from "styled-components";
 
+import { store } from "./store/store";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
@@ -10,20 +12,17 @@ const GlobalContainer = styled.div`
   width: 100%;
   height: 100vh;
   overflow: auto;
-  background: white;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
 `;
 
 const App = () => {
   return (
     <GlobalContainer>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Provider>
     </GlobalContainer>
   );
 };
