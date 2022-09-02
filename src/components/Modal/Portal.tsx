bfe073dd755408b906f7704474213336ce99283f
Parksangwoo <psu8430@gmail.com>
2022-09-02 15:51:41 +0900

    Setting: gitignore 적용

--- src/components/Modal/Portal.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-import { createPortal } from "react-dom";
-
-function Portal({ isOpen, children }: PropsWithChildren<{ isOpen: boolean }>) {
-  if (!isOpen) return null;
-  return createPortal(<div>{children}</div>, document.querySelector("#portal") as Element);
-}
-
-export default Portal;
